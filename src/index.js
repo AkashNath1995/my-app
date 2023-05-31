54a642492542d811b9c5174181ac63ba8f3b35da
Akash <studyforphysics27@gmail.com>
2023-05-31 10:35:28 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-import NavBar from './components/NavBar';import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import App from './App';
 
 
