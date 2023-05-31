d853f20bb3fc406b2f43dd52771d6ec3e6c1451d
Akash <studyforphysics27@gmail.com>
2023-05-31 10:32:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
-const domain = 'dev-j5trf4h88phz0kn6.us.auth0.com';
-const clientId = 'zahDvIEarwa4QVPyFaSgJ5fVwk9r0KBx';
+
 
 ReactDOM.render(
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    >
       <BrowserRouter>
       <Switch>
         <Route exact path="/">
@@ -22,7 +16,7 @@ ReactDOM.render(
       <App/>
       <NavBar />
       </BrowserRouter>
-    </Auth0Provider>
+    
 ,
   document.getElementById('root')
 );
